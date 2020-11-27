3e1cac0939a0a58602ad70f0ab8365552d483040
Prateek Gupta <prateekgupta.3991@gmail.com>
2020-11-27 14:21:16 +0530

    modified configs as modular

---
+++ configs/config.go
@@ -0,0 +1,26 @@
+package configs
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conf struct {
+	ServerPort string
+	CasDb string
+	Keyspace string
+}
+
+func InitConfig(file string) (*Conf, error) {
+	if file, err := os.Open(file); err != nil {  
+		return nil, err 
+	} else {
+		decoder := json.NewDecoder(file)
+		con := new(Conf)
+		if err = decoder.Decode(&con); err != nil {
+			return nil, err
+		} else {
+			return con, nil
+		}
+	}
+}
--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/prateekgupta3991/refresher/cassandra"
+	"github.com/prateekgupta3991/refresher/configs"
 	"github.com/prateekgupta3991/refresher/handlers"
 )
 
@@ -15,8 +16,13 @@ var (
 
 func main() {
 	fmt.Println("Finally back to GO.")
+	var con *configs.Conf
+	var err error
+	if con, err = configs.InitConfig("./configs/conf.dev.json"); err != nil {
+		log.Panic("Error during config initialisation")
+	}
 	CassandraSession := cassandra.Session
 	defer CassandraSession.Close()
 	router.GET("/login", handlers.Login)
-	log.Fatal(router.Run(":8080"))
+	log.Fatal(router.Run(":" + con.ServerPort))
 }
